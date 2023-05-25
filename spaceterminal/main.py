54b4f6fbe17f5a1b56ea8f30e11e93a7cbd52e36
Jason Hutson <110316760+HutsonJason@users.noreply.github.com>
2023-05-25 08:42:07 -0400

    Add start to Register modal. Updated style.

--- spaceterminal/main.py
+++ spaceterminal/main.py
@@ -1,6 +1,7 @@
 import space as s
 from rich.markdown import Markdown
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.containers import Container, Grid, ScrollableContainer
 from textual.screen import ModalScreen
 from textual.widgets import (
@@ -24,18 +25,50 @@ LOGIN_MD = """
 A SpaceTrader API interface. Enter access token to start, or create new account.
 """
 
+REGISTER_MD = """
+
+## Register new agent
+
+Players are called agents, and each agent is identified by a unique call sign, such as ZER0_SH0T or SP4CE_TR4DER. All of your ships, contracts, credits, and other game assets will be associated with your agent identity.
+
+Your starting faction will determine which system you start in, but the default faction should be fine for now.
+"""
+
 
 class LoginScreen(ModalScreen[str]):
+    BINDINGS = [("escape", "app.pop_screen", "Close Login")]
+
     def compose(self) -> ComposeResult:
-        yield LoginContainer()
+        yield LoginContainer(id="modal-login")
 
 
 class LoginContainer(Container):
+    """Container to login with access token, or create new account."""
+
     def compose(self) -> ComposeResult:
         yield Static(Markdown(LOGIN_MD))
         yield Input(placeholder="Access Token", id="input-access-token")
         yield Button("Login", id="button-login", variant="success")
         yield Button("Create account", id="button-create-account", variant="primary")
+
+
+class RegisterScreen(ModalScreen[str]):
+    BINDINGS = [("escape", "app.pop_screen", "Close Register")]
+
+    def compose(self) -> ComposeResult:
+        yield RegisterContainer(id="modal-login")
+
+
+class RegisterContainer(Container):
+    """Container to register new account."""
+
+    def compose(self) -> ComposeResult:
+        yield Static(Markdown(REGISTER_MD))
+        yield Input(placeholder="Call Sign", id="input-symbol")
+        yield Input(placeholder="Starting Faction", value="COSMIC", id="input-faction")
+        yield Button(
+            "Register account", id="button-register-account", variant="success"
+        )
 
 
 class AgentInfo(Static):
@@ -66,9 +99,9 @@ class SpaceApp(App):
     CSS_PATH = "style.css"
     TITLE = "Trading Space"
     BINDINGS = [
-        ("ctrl+t", "app.toggle_dark", "Toggle dark mode"),
-        ("ctrl+c,ctrl+q", "app.quit", "Quit"),
-        ("l", "login", "Login"),
+        Binding("ctrl+c", "app.quit", "Quit", priority=True),
+        Binding("ctrl+t", "app.toggle_dark", "Toggle dark mode"),
+        Binding("l", "login", "Login"),
     ]
 
     def compose(self) -> ComposeResult:
@@ -93,6 +126,9 @@ class SpaceApp(App):
             account.access_token = input_widget.value
             self.pop_screen()
             self.query_one(AgentInfo).update_agent_info()
+        elif button_id == "button-create-account":
+            self.pop_screen()
+            self.push_screen(RegisterScreen())
 
     def action_login(self) -> None:
         """Action to display the login modal."""
