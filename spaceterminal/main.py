dc460bbd4b43f16dbf5533c2e2f72fb2b6584ec3
Jason Hutson <110316760+HutsonJason@users.noreply.github.com>
2023-05-26 14:10:18 -0400

    Basic UI (#2)
    * Added basic UI with textual for a start.
    
    * Add Login modal at startup.
    
    * Added Key bind action to open login modal.
    
    * Add start to Register modal. Updated style.
    
    * Updated layout.
    Added MainTabContainer for styling.
    Changed from rich markdown to textual markdown.
    Changed the way the agent info tab is displayed by adding markdown.
    Removed login tab.
    Added status tab for server status.
    Moved main app rendering of TabbedContent to its own screen class.
    Moved bindings to their own screens.
    Added register agent function, not tested.
    Added get status function for server status.
    
    * Fixed Tab scrolling.
    Tab names now stay fixed to the top of the screen.
    Removed unused MainTabContainer class and its children.
    StatusBody and AgentBody are now directly called in TabPane.
    
    * Updated isort to use black profile.

---
+++ spaceterminal/main.py
@@ -0,0 +1,207 @@
+import space as s
+from textual.app import App, ComposeResult
+from textual.binding import Binding
+from textual.containers import Container
+from textual.screen import ModalScreen, Screen
+from textual.widgets import (
+    Button,
+    Footer,
+    Header,
+    Input,
+    Label,
+    Markdown,
+    Placeholder,
+    Static,
+    TabbedContent,
+    TabPane,
+)
+
+account = s.Account()
+
+LOGIN_MD = """
+
+# Trading Space
+
+A SpaceTrader API interface. Enter access token to start, or create new account.
+"""
+
+REGISTER_MD = """
+
+# Register new agent
+
+Players are called agents, and each agent is identified by a unique call sign, such as ZER0_SH0T or SP4CE_TR4DER. All of your ships, contracts, credits, and other game assets will be associated with your agent identity.
+
+Your starting faction will determine which system you start in, but the default faction should be fine for now.
+"""
+
+
+class LoginScreen(ModalScreen[str]):
+    BINDINGS = [("escape", "app.pop_screen", "Close Login")]
+
+    def compose(self) -> ComposeResult:
+        yield LoginContainer(id="modal-login")
+
+
+class LoginContainer(Container):
+    """Container to login with access token, or create new account."""
+
+    def compose(self) -> ComposeResult:
+        yield Markdown(LOGIN_MD)
+        yield Input(placeholder="Access Token", id="input-access-token")
+        yield Button("Login", id="button-login", variant="success")
+        yield Button("Create account", id="button-create-account", variant="primary")
+
+
+class RegisterScreen(ModalScreen[str]):
+    BINDINGS = [("escape", "app.pop_screen", "Close Register")]
+
+    def compose(self) -> ComposeResult:
+        yield RegisterContainer(id="modal-login")
+
+
+class RegisterContainer(Container):
+    """Container to register new account."""
+
+    def compose(self) -> ComposeResult:
+        yield Markdown(REGISTER_MD)
+        yield Input(placeholder="Call Sign", id="input-symbol")
+        yield Input(placeholder="Starting Faction", value="COSMIC", id="input-faction")
+        yield Button(
+            "Register account", id="button-register-account", variant="success"
+        )
+
+
+class AgentBody(Static):
+    agent_markdown = Markdown()
+
+    def on_mount(self) -> None:
+        self.update_agent_info()
+
+    def compose(self) -> ComposeResult:
+        yield self.agent_markdown
+
+    def update_agent_info(self) -> None:
+        agent = s.get_agent(account.header)
+        if "error" in agent:
+            agent_md = f"""
+{agent['error']['message']}
+
+Code: {agent['error']['code']}"""
+            self.agent_markdown.update(agent_md)
+        else:
+            agent = s.get_agent(account.header)["data"]
+            agent_md = f"""
+Account ID: {agent["accountId"]}
+
+Symbol: {agent["symbol"]}
+
+Headquarters: {agent["headquarters"]}
+
+Credits: {agent["credits"]}
+
+Starting Faction: {agent["startingFaction"]}"""
+            self.agent_markdown.update(agent_md)
+
+
+class StatusBody(Static):
+    """Body content of the status tab."""
+
+    status_markdown = Markdown()
+
+    def on_mount(self):
+        self.update_status()
+
+    def compose(self) -> ComposeResult:
+        yield self.status_markdown
+
+    def update_status(self):
+        response = s.get_status()
+        response_code = response.status_code
+        status = s.get_status().json()
+
+        if response_code == 200:
+            self.status_markdown.add_class("online")
+            status_md = f"""
+# Status: {status["status"]}
+
+Version: {status["version"]}
+
+Description: {status["description"]}
+
+#### Stats
+
+Agents: {status["stats"]["agents"]}
+
+Ships: {status["stats"]["ships"]}
+
+Systems: {status["stats"]["systems"]}
+
+Waypoints: {status["stats"]["waypoints"]}
+
+#### Server Resets
+
+Reset Date: {status["resetDate"]}
+
+Next: {status["serverResets"]["next"]}
+
+Frequency: {status["serverResets"]["frequency"]}
+"""
+            self.status_markdown.update(status_md)
+
+        else:
+            self.status_markdown.add_class("offline")
+            status_md = f"""
+# {status["error"]["message"]}
+
+{status["error"]["code"]}
+"""
+            self.status_markdown.update(status_md)
+
+
+class SpaceApp(App):
+    CSS_PATH = "style.css"
+    TITLE = "Trading Space"
+    BINDINGS = [Binding("ctrl+c", "app.quit", "Quit", priority=True, show=False)]
+
+    class MainScreen(Screen):
+        BINDINGS = [
+            ("escape", "app.quit", "Quit"),
+            ("ctrl+t", "app.toggle_dark", "Toggle dark mode"),
+            ("l", "login", "Login"),
+        ]
+
+        def compose(self) -> ComposeResult:
+            yield Header(show_clock=True)
+            with TabbedContent(initial="status"):
+                with TabPane("Status", id="status"):
+                    yield StatusBody()
+                with TabPane("Agent", id="agent"):
+                    yield AgentBody()
+                with TabPane("Ships", id="ships"):
+                    yield Static("Ships")
+            yield Footer()
+
+    def on_mount(self):
+        self.push_screen(self.MainScreen())
+        self.push_screen(LoginScreen())
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        button_id = event.button.id
+        if button_id == "button-login":
+            input_widget = self.query_one("#input-access-token", Input)
+            account.access_token = input_widget.value
+            self.pop_screen()
+            self.query_one(AgentBody).update_agent_info()
+        elif button_id == "button-create-account":
+            self.pop_screen()
+            self.push_screen(RegisterScreen())
+
+    def action_login(self) -> None:
+        """Action to display the login modal."""
+
+        self.push_screen(LoginScreen())
+
+
+if __name__ == "__main__":
+    app = SpaceApp()
+    app.run()
---
+++ spaceterminal/space.py
@@ -0,0 +1,76 @@
+import json
+
+import requests
+
+api_url = "https://api.spacetraders.io/v2/"
+register_url = f"{api_url}register/"
+agent_url = f"{api_url}my/agent/"
+
+
+class Account:
+    """Holds the account access token and the header for API calls."""
+
+    def __init__(self, access_token: str = None):
+        self.access_token = access_token
+
+    @property
+    def header(self):
+        return {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.access_token}",
+        }
+
+    @property
+    def access_token(self):
+        return self._access_token
+
+    @access_token.setter
+    def access_token(self, access_token):
+        self._access_token = access_token
+
+
+def get_agent(header):
+    """Gets My Agent info.
+
+    https://api.spacetraders.io/v2/my/agent
+
+    Args:
+        header: The API request header with the access token.
+
+    Returns:
+        {
+          "data": {
+            "accountId": "string",
+            "symbol": "string",
+            "headquarters": "string",
+            "credits": 0,
+            "startingFaction": "string"
+          }
+        }
+    """
+    return requests.get(agent_url, headers=header).json()
+
+
+def register_agent(symbol: str, faction: str = "COSMIC"):
+    """Registers a new agent.
+
+    https://api.spacetraders.io/v2/register
+
+    Args:
+        symbol: The unique call sign associated with agent identity.
+        faction: The starting faction, which determines which system you start in.
+
+    Returns:
+        json data with all the new account details.
+
+    """
+    header = {
+        "Content-Type": "application/json",
+    }
+    payload = {"symbol": symbol, "faction": faction}
+
+    return requests.post(register_url, json=payload, headers=header).json()
+
+
+def get_status():
+    return requests.get(api_url)
