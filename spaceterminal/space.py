afc843cc9d109d2295f43b295bb0b8721c5febed
Jason Hutson <110316760+HutsonJason@users.noreply.github.com>
2023-06-10 17:30:26 -0400

    Add Client testing. Removed unused get_agent and register_agent from space.py.

--- spaceterminal/space.py
+++ spaceterminal/space.py
@@ -1,54 +1,9 @@
 import requests
 
 api_url = "https://api.spacetraders.io/v2/"
-register_url = f"{api_url}register/"
-agent_url = f"{api_url}my/agent/"
 factions_url = f"{api_url}factions/"
 my_ships_url = f"{api_url}my/ships/"
 my_contracts_url = f"{api_url}my/contracts/"
-
-
-# def get_agent(client):
-#     """Gets My Agent info.
-#
-#     https://api.spacetraders.io/v2/my/agent
-#
-#     Args:
-#         client: The Client object with session.
-#
-#     Returns:
-#         {
-#           "data": {
-#             "accountId": "string",
-#             "symbol": "string",
-#             "headquarters": "string",
-#             "credits": 0,
-#             "startingFaction": "string"
-#           }
-#         }
-#     """
-#     return client.session.get(agent_url).json()
-
-
-def register_agent(symbol: str = "", faction: str = "COSMIC"):
-    """Registers a new agent.
-
-    https://api.spacetraders.io/v2/register
-
-    Args:
-        symbol: The unique call sign associated with agent identity.
-        faction: The starting faction, which determines which system you start in.
-
-    Returns:
-        json data with all the new account details.
-
-    """
-    header = {
-        "Content-Type": "application/json",
-    }
-    payload = {"symbol": symbol, "faction": faction}
-
-    return requests.post(register_url, json=payload, headers=header)
 
 
 def get_status():
